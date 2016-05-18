965850bcb0affe7c39496d5e898b2813a7d678fb
aryrobocode <ang.ray.yan@gmail.com>
2016-05-18 19:28:18 +0800

    More stable CSS + structural modifications

--- _site/js/navmenu-hide.js
+++ _site/js/navmenu-hide.js
@@ -1,5 +1,12 @@
 $(function() {
-	// Automatically hide nav-menu after click on mobile
-	$("#sgc-navbar li a").on('click', function() {
+	$('#sgc-navbar').on('click', function() {
+		$(this).collapse('hide');
 	});
+});
+
+$(document).on('click','#sgc-navbar',function(e) {
+	alert("A");
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
 });
